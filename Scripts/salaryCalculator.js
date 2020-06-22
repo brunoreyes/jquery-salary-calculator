f400056de30c8b617f1d353435c0b608a9cfa7a9
“brunoreyes” <brunoreyes@Brunos-MacBook-Pro.local>
2020-06-21 20:46:06 -0500

    Revised total monthly salary button

--- Scripts/salaryCalculator.js
+++ Scripts/salaryCalculator.js
@@ -1,13 +1,13 @@
 //Here I'm making sure the JavaScript file has been sourced properly
-console.log(`Hello from JavaScript!`);
-
+//console.log(`Hello from JavaScript!`);
 
 let employeeInfo = [];
+
 //Here I'm making sure the jQuery file has been sourced properly
 $(document).ready(onReady);
 
 function onReady() {
-    console.log(`Hello from jQuery!`);
+    //console.log(`Hello from jQuery!`);
 
     let el = $('#employeeInfoOutput');
     //2. target id: employeeInfoOutput
@@ -19,22 +19,9 @@ function onReady() {
     //when id: addInfoButton is clicked we addEmployeeInfo
     $('#addInfoButton').on('click', addEmployeeInfo);
 
-    //   $('#addInfoButton').on('click', animate);
-
-    //addCar is 
     displayEmployeeInfo();
+
 };//end readyNow
-
-
-{/* <input type="text" placeholder="First Name" id="firstNameInput" alt="first name input">
-                <input type="text" placeholder="Last Name" id="lastNameInput" alt="last name input">
-                <input type="number" placeholder="ID" id="idInput" alt="id input">
-                <input type="text" placeholder="Title" id="titleInput" alt="title input">
-                <input type="number" placeholder="Annual Salary" id="annualSalaryInput" alt="annual salary input">
-                <button type="submit" id="submitButton">Submit</button> */}
-
-
-
 
 function newEmployeeData(firstNameInput, lastNameInput, idInput, titleInput, annualSalaryInput) {
     console.log('in newEmployeData:', firstNameInput, lastNameInput, idInput, titleInput, annualSalaryInput);
@@ -46,58 +33,65 @@ function newEmployeeData(firstNameInput, lastNameInput, idInput, titleInput, ann
         annualSalary: annualSalaryInput
     }
     employeeInfo.push(newEmployeeDataObject);
-    //disableForm();
-    // disableForm() checks if max spots reached, if so, disables form
-    // totalPrice();
-    // totalPrice() adds up the total salary of all employees
+    totalMonthlySalary()
     return true;
 } // end newEmployeData
 
 console.log(`----------newEmployeeData() Test----------`);
 console.log(employeeInfo);
-console.log(newEmployeeData('Jimmy', 'Fallon', 1434, 'Show Host', 400000));
-console.log(newEmployeeData('Al', `Pacino`, 1000, 'Movie Actor', 2000000));
-console.log(newEmployeeData('Bruno', 'Reyes', 1434, 'Future Developer', 16000));
+console.log(newEmployeeData('Jimmy', 'Fallon', 1434, 'Host', 400000));
+console.log(newEmployeeData('Al', `Pacino`, 1000, 'Actor', 2000000));
+console.log(newEmployeeData('Bruno', 'Reyes', 1494, 'Developer', 16000));
 console.log(employeeInfo);
 
 
 function addEmployeeInfo() {
-    newEmployeeData($('.firstNameInput').val(), $('.lastNameInput').val(), $('.idInput').val(), $('.titleInput').val(), $('.annualSalaryInput').val());
+    //You can only change the value of classes
+    newEmployeeData($('#firstNameInput').val(), $('#lastNameInput').val(), $('#idInput').val(), $('#titleInput').val(), $('#annualSalaryInput').val());
     //these values are taking the place of the org. values of newCar
-    $('.firstNameInput').val('')
-    $('.lastNameInput').val('')
-    $('.idInput').val('')
-    $('.titleInput').val('')
-    $('.annualSalaryInput').val('')
+    $('#firstNameInput').val('')
+    $('#lastNameInput').val('')
+    $('#idInput').val('')
+    $('#titleInput').val('')
+    $('#annualSalaryInput').val('')
     displayEmployeeInfo();
 }// end addEmployeeInfo
 
 function displayEmployeeInfo() {
-    let el = $('#employeeInfoOutput');
-    el.empty();
+    $('#employeeInfoOutput').empty();
     for (employeeObject of employeeInfo) {
-        el.append(`<tr><td class="firstNameColumn">` + employeeObject.firstName + `</td>
-        <td class="lastNameColumn">` + employeeObject.lastName + `</td>
-        <td class="idColumn">` + employeeObject.id + `</td>
-        <td class="titleColumn">` + employeeObject.title + `</td>
-        <td class="annualSalaryColumn">` + employeeObject.annualSalary + `</td>
-        <td class="deleteColumn"><button id="deleteButton">Delete</button></td></tr>`);
+        $('#employeeInfoOutput').append(`<tr><td class="firstNameColumn">` + employeeObject.firstName + `</td>` +
+            `<td id="lastNameColumn">` + employeeObject.lastName + `</td>` +
+            `<td id="idColumn">` + employeeObject.id + `</td>` +
+            `<td id="titleColumn">` + employeeObject.title + `</td>` +
+            `<td id="annualSalaryColumn">` + employeeObject.annualSalary + `</td>` +
+            `<td id="deleteColumn"><button id="deleteButton">Delete</button></td></tr>`);
         // here I'm creating a table row filled with info of each employee
     }// end for 
 }//end displayEmployeeInfo
 
-console.log(employeeInfo);
+function totalMonthlySalary() {
+    let totalValue = 0;
+    for (employeeObject of employeeInfo) {
+        totalValue += employeeObject.annualSalary;
+    }
+    console.log(totalValue)
+    SalaryColor(totalValue)
+    // let convertedMonthlySalary = (new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(totalValue));
+    // $(`#totalMonthlySalary`).append(`` + convertedMonthlySalary + ``)
 
+}//end totalMonthlySalary
 
-const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2
-})
+function SalaryColor(totalValue) {
+    if (totalValue > 20000) {
+        $('#totalMonthlySalaryRed').text(`$${totalValue}`);
+        return false
+    }
+    else {
+        $('#totalMonthlySalary').text(`$${totalValue}`);
+    }
+}
 
-formatter.format(1000) // "$1,000.00"
-formatter.format(10) // "$10.00"
-formatter.format(123233000) // "$123,233,000.00"
 
 
 // [X] The application should have an input form that collects
@@ -106,7 +100,7 @@ formatter.format(123233000) // "$123,233,000.00"
 // [X] A 'Submit' button should collect the form information, store the
 // information to calculate monthly costs, append information to the DOM and clear the input fields. 
 
-// [] Using the stored information, calculate monthly costs and append this
+// [X] Using the stored information, calculate monthly costs and append this
 // to the to DOM. If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
 
 // [] Create a delete button that removes an employee from the DOM. For Base
@@ -114,7 +108,7 @@ formatter.format(123233000) // "$123,233,000.00"
 
 // ## Stretch Mode
 
-// [] Add styling or extra functionality that fits with the theme of this assignment.
+// [X] Add styling or extra functionality that fits with the theme of this assignment.
 
 // [] Once the employee is deleted, update the total spend on salaries account for 
 //this employee's removal. This will require that the logic knows which element was removed. You will need to use `.text()` as a getter or look into jQuery's `.data()` function. This is tricky!
